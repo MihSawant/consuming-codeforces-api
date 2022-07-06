6fb947a0efa2cc321da675ef2e2532018ba1efd1
Mihir Sawant <mih3190Sawant@outlook.com>
2022-07-07 01:06:52 +0530

    consuming data using rest-template

--- src/main/java/sawant/mihir/consumingrestopenfeign/config/FeignClientConfig.java
+++
@@ -1,9 +0,0 @@
-package sawant.mihir.consumingrestopenfeign.config;
-
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableFeignClients(basePackages = "sawant.mihir.consumingrestopenfeign.proxy")
-public class FeignClientConfig {
-}
--- src/main/java/sawant/mihir/consumingrestopenfeign/controller/CodeforcesApiController.java
+++ src/main/java/sawant/mihir/consumingrestopenfeign/controller/CodeforcesApiController.java
@@ -1,36 +1,59 @@
 package sawant.mihir.consumingrestopenfeign.controller;
 
+import feign.Response;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 import sawant.mihir.consumingrestopenfeign.model.Member;
 import sawant.mihir.consumingrestopenfeign.model.ProblemSetStatus;
-import sawant.mihir.consumingrestopenfeign.proxy.ProblemStatusProxy;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-public record CodeforcesApiController(ProblemStatusProxy proxy) {
+public class CodeforcesApiController {
 
-    public CodeforcesApiController(ProblemStatusProxy proxy) {
-        this.proxy = proxy;
+    @Value("${service.url}")
+    private String url;
+    private final RestTemplate restTemplate;
+    public CodeforcesApiController(RestTemplate restTemplate){
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/status")
-    public ProblemSetStatus recentProblemStatus(@RequestParam int count) {
-        return proxy.getProblemSetStatus(count);
+    public ResponseEntity<?> recentProblemStatus(@RequestParam int count) {
+        String uri = url + "?count=" +count;
+
+        try{
+            ProblemSetStatus problemStatus = restTemplate
+                    .exchange(uri, HttpMethod.GET, HttpEntity.EMPTY, ProblemSetStatus.class)
+                    .getBody();
+            return ResponseEntity.status(HttpStatus.ACCEPTED)
+                    .body(problemStatus);
+        }catch(HttpClientErrorException ex){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(ex.getMessage());
+        }
+
     }
 
     @GetMapping("/members")
-    public ResponseEntity<List<Member[]>> getAllTheMembers(@RequestParam int count) {
-        ProblemSetStatus problemSetStatus = recentProblemStatus(count);
-        var members = Arrays.stream(problemSetStatus.result())
-                .map(result -> result.author().members())
-                .collect(Collectors.toList());
-
-        return ResponseEntity.status(HttpStatus.ACCEPTED)
-                .body(members);
+    public ResponseEntity<?> getAllTheMembers(@RequestParam int count) {
+        Object response = recentProblemStatus(count).getBody();
+        if(response instanceof ProblemSetStatus problemSetStatus){
+             var members = Arrays.stream(problemSetStatus.result())
+                     .map(result -> result.author().members()).toList();
+             return ResponseEntity.status(HttpStatus.ACCEPTED)
+                     .body(members);
+         } else{
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body((String) response);
+         }
     }
 }
